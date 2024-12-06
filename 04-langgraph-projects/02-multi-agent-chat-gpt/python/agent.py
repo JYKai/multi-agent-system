14ac0a0e01f238a47491709ceb4076158d44cb99
Jiyun <noahyun1222@gmail.com>
2024-12-06 15:51:59 +0900

    Add: Multi-agent build chat-gpt

---
+++ 04-langgraph-projects/02-multi-agent-chat-gpt/python/agent.py
@@ -0,0 +1,25 @@
+from utils.nodes import create_chatbot
+from utils.tools import get_tools
+
+from langgraph.graph import START, StateGraph, MessagesState
+from langgraph.prebuilt import ToolNode, tools_condition
+
+def create_agent(docs_info=None, retriever_tool=None):
+    graph_builder = StateGraph(MessagesState)
+    
+    # chatbot 노드 생성시 docs_info와 retriever_tool 전달
+    chatbot_node = create_chatbot(docs_info, retriever_tool)
+    graph_builder.add_node("chatbot", chatbot_node)
+    
+    # tool_node도 여기서 생성
+    tool_node = ToolNode(tools=get_tools(retriever_tool))
+    graph_builder.add_node("tools", tool_node)
+    
+    graph_builder.add_conditional_edges(
+        "chatbot",
+        tools_condition,
+    )
+    graph_builder.add_edge("tools", "chatbot")
+    graph_builder.add_edge(START, "chatbot")
+    graph = graph_builder.compile()
+    return graph
